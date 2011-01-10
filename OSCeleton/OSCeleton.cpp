340297e10d29976ec4d29a3cbd9465d24291ee0c
Tony Gonçalves <tony@sensebloom.com>
2011-01-10 11:38:28 +0000

    Added wishlist. Added GPL license.

--- OSCeleton/OSCeleton.cpp
+++ OSCeleton/OSCeleton.cpp
@@ -1,3 +1,23 @@
+/***********************************************************************
+
+    OSCeleton - OSC proxy for kinect skeleton data.
+    Copyright (C) <2010>  <Sensebloom lda.>
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+***********************************************************************/
+
 #include <stdio.h>
 
 #include <XnCppWrapper.h>
